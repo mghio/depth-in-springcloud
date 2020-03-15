c799aa2cbf0832a8b227fcde349d57a400b039a5
maguihai <guihai.dev@gmail.com>
2020-03-15 09:35:28 +0800

    initial commit.

---
+++ service-consumer/src/main/java/cn/mghio/serviceconsumer/ServiceConsumerApplication.java
@@ -0,0 +1,15 @@
+package cn.mghio.serviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceConsumerApplication.class, args);
+  }
+
+}
---
+++ service-provider/src/test/java/cn/mghio/serviceprovider/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mghio.serviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
